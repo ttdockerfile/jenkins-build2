3704abaf057d1f2dbeef19f0ee1b493b30e23f46
buyfakett <buyfakett@vip.qq.com>
2024-06-06 18:48:09 +0800

    修改版本错误bug

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ jenkins_version=2.403
 if [ ! -f apache-maven-3.6.3-bin.zip ];then
     wget https://dlcdn.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
 fi
-if [ ! -f node-v16.20.0-linux-x64.tar.xz ];then
+if [ ! -f node-v16.20.2-linux-x64.tar.xz ];then
     wget https://gitee.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v16.20.2-linux-x64.tar.xz
 fi
 if [ ! -f node-v14.17.1-linux-x64.tar.xz ];then
