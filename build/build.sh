3d9a004e856b01ad43dc1f9326e602d188ecb53c
buyfakett <buyfakett@vip.qq.com>
2024-09-02 23:07:05 +0800

    初版

--- build/build.sh
+++ build/build.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-jenkins_version=2.470
-python3_version=3.12.3
+jenkins_version=2.474
 golang_version=1.22.5
 
 if [ ! -f apache-maven-3.6.3-bin.zip ];then
     wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/apache-maven-3.6.3-bin.zip
 fi
-if [ ! -f jdk-21.0.2_linux-x64_bin.rpm ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/jdk-21.0.2_linux-x64_bin.rpm
+if [ ! -f node-v20.17.0-linux-x64.tar.xz ];then
+    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v20.17.0-linux-x64.tar.xz
 fi
-if [ ! -f node-v16.20.2-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v16.20.2-linux-x64.tar.xz
+if [ ! -f node-v18.20.4-linux-x64.tar.xz ];then
+    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v18.20.4-linux-x64.tar.xz
+fi
+if [ ! -f node-v17.5.0-linux-x64.tar.xz ];then
+    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v17.5.0-linux-x64.tar.xz
+fi
+if [ ! -f node-v16.20.0-linux-x64.tar.xz ];then
+    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v16.20.0-linux-x64.tar.xz
 fi
 if [ ! -f node-v14.17.1-linux-x64.tar.xz ];then
     wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v14.17.1-linux-x64.tar.xz
 fi
-if [ ! -f node-v12.4.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v12.4.0-linux-x64.tar.xz
-fi
-if [ ! -f node-v10.23.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v10.23.0-linux-x64.tar.xz
-fi
-if [ ! -f Python-${python3_version}.tgz ];then
-    wget https://www.python.org/ftp/python/${python3_version}/Python-${python3_version}.tgz
+if [ ! -f node-v10.24.1-linux-x64.tar.xz ];then
+    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v10.24.1-linux-x64.tar.xz
 fi
 if [ ! -f go${golang_version}.linux-amd64.tar.gz ];then
     wget https://go.dev/dl/go${golang_version}.linux-amd64.tar.gz
 fi
 if [ ! -f jenkins-${jenkins_version}.war ];then
     # wget jenkins-${jenkins_version}.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/${jenkins_version}/jenkins.war
-    wget -O jenkins-${jenkins_version}.war https://get.jenkins.io/war-stable/${jenkins_version}/jenkins.war
+    # wget -O jenkins-${jenkins_version}.war https://get.jenkins.io/war-stable/${jenkins_version}/jenkins.war
+    wget -O jenkins-${jenkins_version}.war https://get.jenkins.io/war/${jenkins_version}/jenkins.war
 fi
 
-docker build --build-arg JENKINS_VERSION=${jenkins_version} --build-arg PYTHON3_VERSION=${python3_version} --build-arg GOLANG_VERSION=${golang_version} -t buyfakett/jenkins:${jenkins_version} .
+docker build --build-arg JENKINS_VERSION=${jenkins_version} --build-arg --build-arg GOLANG_VERSION=${golang_version} -t buyfakett/jenkins:${jenkins_version} .
