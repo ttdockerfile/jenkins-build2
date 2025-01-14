43cd81ed84e8a6fcbaa03d270a37c9d78274809d
buyfakett <buyfakett@vip.qq.com>
2025-01-14 16:25:59 +0800

    修改脚本

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-JENKINS_VERSION=2.452.1
+JENKINS_VERSION=2.475
 
-npm i -g node-sass --sass_binary_site=https://registry.npmmirror.com/mirrors/node-sass/
-npm i -g miniprogram-ci --registry=https://registry.npmmirror.com
 
-pip3 install -r /data/app/python/requirements.txt
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
-export JAVA_HOME=/usr/lib/jvm/jdk-21-oracle-x64
-
-/usr/lib/jvm/jdk-21-oracle-x64/bin/java -Duser.timezone=GMT+8 -Dfile.encoding=utf-8 -jar \
--server -Xmx512m -Xms512m -Xss256K \
+/usr/lib/jvm/java-21-openjdk-amd64/bin/java -Duser.timezone=GMT+8 -Dfile.encoding=utf-8 -jar \
 jenkins-${JENKINS_VERSION}.war --httpPort=8080
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo 'Asia/Shanghai' > /etc/timezone
 
 port=8081
-jenkins_version=2.452.1
+jenkins_version=2.475
 
 docker kill jenkins-${port}
 docker rm jenkins-${port}
@@ -24,4 +24,3 @@ docker run -d --name jenkins-${port} \
 -v ./data-${port}:/data/app/jenkins/data \
 registry.cn-hangzhou.aliyuncs.com/buyfakett/jenkins:${jenkins_version}
 
-# -v ./run.sh:/data/app/jenkins/run.sh \
