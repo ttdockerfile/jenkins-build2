02b590482b5b6c703fb57a031d12d43c478bc407
buyfakett <buyfakett@vip.qq.com>
2026-01-09 09:02:18 +0000

    静默下载

--- build/auto_build.sh
+++ build/auto_build.sh
@@ -4,39 +4,39 @@ jenkins_version=$1
 golang_version=$2
 
 if [ ! -f apache-maven-3.6.3-bin.zip ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/apache-maven-3.6.3-bin.zip
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/apache-maven-3.6.3-bin.zip
 fi
 if [ ! -f node-v24.11.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v24.11.0-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v24.11.0-linux-x64.tar.xz
 fi
 if [ ! -f node-v22.14.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v22.14.0-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v22.14.0-linux-x64.tar.xz
 fi
 if [ ! -f node-v20.17.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v20.17.0-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v20.17.0-linux-x64.tar.xz
 fi
 if [ ! -f node-v18.20.4-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v18.20.4-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v18.20.4-linux-x64.tar.xz
 fi
 if [ ! -f node-v17.5.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v17.5.0-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v17.5.0-linux-x64.tar.xz
 fi
 if [ ! -f node-v16.20.0-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v16.20.0-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v16.20.0-linux-x64.tar.xz
 fi
 if [ ! -f node-v14.17.1-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v14.17.1-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v14.17.1-linux-x64.tar.xz
 fi
 if [ ! -f node-v10.24.1-linux-x64.tar.xz ];then
-    wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v10.24.1-linux-x64.tar.xz
+    wget -q https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/node-v10.24.1-linux-x64.tar.xz
 fi
 if [ ! -f go${golang_version}.linux-amd64.tar.gz ];then
-    wget https://go.dev/dl/go${golang_version}.linux-amd64.tar.gz
+    wget -q https://go.dev/dl/go${golang_version}.linux-amd64.tar.gz
 fi
 if [ ! -f jenkins-${jenkins_version}.war ];then
-    # wget jenkins-${jenkins_version}.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/${jenkins_version}/jenkins.war
-    # wget -O jenkins-${jenkins_version}.war https://get.jenkins.io/war-stable/${jenkins_version}/jenkins.war
-    wget -O jenkins-${jenkins_version}.war https://get.jenkins.io/war/${jenkins_version}/jenkins.war
+    # wget -q jenkins-${jenkins_version}.war https://mirrors.tuna.tsinghua.edu.cn/jenkins/war/${jenkins_version}/jenkins.war
+    # wget -q -O jenkins-${jenkins_version}.war https://get.jenkins.io/war-stable/${jenkins_version}/jenkins.war
+    wget -q -O jenkins-${jenkins_version}.war https://get.jenkins.io/war/${jenkins_version}/jenkins.war
 fi
 
 rm -rf go/ maven/
