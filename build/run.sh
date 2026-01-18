4d735e722fc800a8cf3ba2c1dc63dd9b5b8932d4
buyfakett <buyfakett@vip.qq.com>
2026-01-18 11:31:34 +0000

    增加默认端口

--- build/run.sh
+++ build/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PORT="${PORT:-8080}"
+
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 /usr/lib/jvm/java-25-openjdk-amd64/bin/java -Duser.timezone=GMT+8 -Dfile.encoding=utf-8 -jar \
