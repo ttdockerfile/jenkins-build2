64a72c24bd7d71f0fe276a863041ddc86dd87c9c
buyfakett <buyfakett@vip.qq.com>
2026-01-18 09:11:20 +0000

    更新go

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-jenkins_version=2.527
-golang_version=1.25.1
+jenkins_version=2.546
+golang_version=1.25.6
 
 if [ ! -f apache-maven-3.6.3-bin.zip ];then
     wget https://github.com/buyfakett/centos7_initialization/releases/download/v1.2.3/apache-maven-3.6.3-bin.zip
